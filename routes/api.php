4b20423b7ca1cd6440d796500925f9908343ab69
Itamar Chaves <109184505+Itamar-Ichaves@users.noreply.github.com>
2023-05-30 20:44:59 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,16 @@ use App\Http\Controllers\OrderService\{
 
 use Illuminate\Support\Facades\Route;
 
+Route::group([
+    'prefix' => 'v1',
+],
+function () {
 /**
  * Route Orders Services
  */
+
 Route::get('/getorderservice', GetOrderServiceController::class);
 Route::get('/getorderservice/{identify}', GetShowOrderServiceController::class);
 Route::post('/createorderservice', CreateOrderServiceController::class);
 
+});
